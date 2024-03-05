9d30ddd3fa7aa4a9bf3c395eb9689e49d7062fc7
Akaki Mikaia <akakimikaia@gmail.com>
2024-03-05 16:06:15 +0000

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:test/hello.test.ts

--- src/hello.ts
+++ src/hello.ts
@@ -1,5 +1,8 @@
 export const hello: () => 'hello' = () => 'hello';
 
 export function fizzbuzz(n: number): string {
+  if (n % 3 === 0) {
+    return 'Fizz';
+  }
 	return n.toString();
 }
--- test/hello.test.ts
+++ test/hello.test.ts
@@ -1,13 +1,22 @@
 import {fizzbuzz} from '../src/hello';
 
-describe('hello world', () => {
+describe('Positive integers and outputs their string representation', () => {
 	it.each([
 		[1, '1'],
 		[2, '2'],
 		[4, '4'],
-		[5, '5'],
-	])('should return stringified number %i', (arg: number, expected: string) => {
+	])('should return stringified number %i is %s', (arg: number, expected: string) => {
 		const result = fizzbuzz(arg);
 		expect(result).toBe(expected);
 	});
+  describe(' If the number is a multiple of three, return the string "Fizz"', () => {
+    it.each([
+      [3, 'Fizz'],
+      [6, 'Fizz'],
+      [9, 'Fizz'],
+    ])('should return stringified number %i is %s', (arg: number, expected: string) => {
+      const result = fizzbuzz(arg);
+      expect(result).toBe(expected);
+    })
+  })
 });
